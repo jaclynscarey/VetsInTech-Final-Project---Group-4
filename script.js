06260f2bce8bd5ad052e9fada1ae4857c4078e6c
Jaclyn C <109121563+jaclynscarey@users.noreply.github.com>
2023-03-29 15:47:08 +0000

    Group project; carousel, header, nav, and main were each done by a student. My part was the main and joining everyone's code.

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const button = document.getElementById('post-button');
+const buttonEl = document.getElementById('post-button');
 
 function addMsg() {
     let userNameInput = document.getElementById('user-name').value;
     let newMessageInput = document.getElementById('msg-input').value;
+
+    let photoInput = document.getElementById('upload').files;
+    let imageArray = [];
+    imageArray.push(photoInput[0]);
+
     const feedUl = document.getElementById('feed');
     const date = new Date().toLocaleDateString();
     if (userNameInput === '' || newMessageInput === '') {
         alert('Please enter your name and review before trying to post.')
-    } else {
+    } else if (photoInput.length === 0) {
         let liEl = document.createElement('li');
         liEl.setAttribute('class', 'msg');
         liEl.innerHTML = '<strong>' + userNameInput + '</strong> - ' + date + '<br><br>' + newMessageInput;    
         feedUl.append(liEl);
         document.getElementById('user-name').value = '';
         document.getElementById('msg-input').value = '';
+    } else {
+        const image = `<br><br><img class="user-image" src="${URL.createObjectURL(imageArray[0])}">`;
+        let liEl = document.createElement('li');
+        liEl.setAttribute('class', 'msg');
+        liEl.innerHTML = '<strong>' + userNameInput + '</strong> - ' + date + '<br><br>' + newMessageInput + image;    
+        feedUl.append(liEl);
+        document.getElementById('user-name').value = '';
+        document.getElementById('msg-input').value = '';
     }    
 }
 
-button.addEventListener('click', addMsg);
+buttonEl.addEventListener('click', addMsg);
